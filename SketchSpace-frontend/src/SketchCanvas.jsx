4e15c20670c4c932ed7ffa20cf2bad4c7da91591
Sandip Roy <sandiproyofficial29@gmail.com>
2025-12-19 12:40:00 +0530

    Add a button to create some random box.

--- SketchSpace-frontend/src/SketchCanvas.jsx
+++ SketchSpace-frontend/src/SketchCanvas.jsx
@@ -6,8 +6,28 @@ const SketchCanvas = () => {
 
   const [scale, setScale] = useState(1);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
-  const [panning, setPanning] = useState(false);
   const [startPan, setStartPan] = useState({ x: 0, y: 0 });
+  const [elements, setElements] = useState([
+    {
+      id: 1,
+      type: "rect",
+      x: 100,
+      y: 100,
+      width: 100,
+      height: 100,
+      color: "red",
+    },
+    {
+      id: 2,
+      type: "rect",
+      x: 300,
+      y: 200,
+      width: 150,
+      height: 100,
+      color: "blue",
+    },
+  ]);
+  const [isPanning, setIsPanning] = useState(false);
 
   // ================= DRAW =================
   useLayoutEffect(() => {
@@ -38,15 +58,19 @@ const SketchCanvas = () => {
 
     ctx.stroke();
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(100, 250, 200, 200);
+    elements.forEach((el) => {
+      ctx.fillStyle = el.color;
+      if (el.type == "rect") {
+        ctx.fillRect(el.x, el.y, el.width, el.height);
+      }
+    });
 
     ctx.restore();
-  }, [scale, offset]);
+  }, [scale, offset, elements]);
 
   // ================= PAN =================
   const handleMouseDown = (e) => {
-    setPanning(true);
+    setIsPanning(true);
     setStartPan({
       x: e.clientX - offset.x,
       y: e.clientY - offset.y,
@@ -54,15 +78,17 @@ const SketchCanvas = () => {
   };
 
   const handleMouseMove = (e) => {
-    if (!panning) return;
-
-    setOffset({
-      x: e.clientX - startPan.x,
-      y: e.clientY - startPan.y,
-    });
+    if (isPanning) {
+      setOffset({
+        x: e.clientX - startPan.x,
+        y: e.clientY - startPan.y,
+      });
+    } else {
+      return;
+    }
   };
 
-  const handleMouseUp = () => setPanning(false);
+  const handleMouseUp = () => setIsPanning(false);
 
   // ================= ZOOM =================
   const handleWheel = (e) => {
@@ -81,17 +107,40 @@ const SketchCanvas = () => {
     });
   };
 
+  const addRandomShape = () => {
+    const newShaps = {
+      id: Date.now(),
+      type: "rect",
+      x: Math.random() * 500,
+      y: Math.random() * 500,
+      width: Math.random() * 100,
+      height: Math.random() * 100,
+      color: `hsl(${Math.random() * 360}, 70%, 50%)`, // Random Color
+    };
+    setElements([...elements, newShaps]);
+  };
+
   return (
-    <canvas
-      ref={canvasRef}
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onWheel={handleWheel}
-      style={{ display: "block", cursor: panning ? "grabbing" : "grab" }}
-    />
+    <>
+      <div style={{ position: "fixed", top: 10, left: 10, zIndex: 10 }}>
+        <button
+          onClick={addRandomShape}
+          style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+        >
+          + Add Random Box
+        </button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onWheel={handleWheel}
+        style={{ display: "block", cursor: isPanning ? "grabbing" : "grab" }}
+      />
+    </>
   );
 };
 
